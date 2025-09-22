e249e4c78ad1a8d0f113cd4b5b51c28df9899724
furantuturu <furantuturu@proton.me>
2025-09-22 23:08:56 +0800

    elevation from 6 to 5

--- components/fab/FloorsSelectionPerCategoryModal.tsx
+++ components/fab/FloorsSelectionPerCategoryModal.tsx
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.12,
         shadowRadius: 6,
         shadowOffset: { width: 0, height: 2 },
-        elevation: 6,
+        elevation: 5,
     },
     text: {
         fontWeight: "bold",
