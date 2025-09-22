03f95fcad57ccd83a5421664128b17faabce1095
furantuturu <furantuturu@proton.me>
2025-09-22 23:28:27 +0800

    rewrote marker image path

--- constants/markerConfig.ts
+++ constants/markerConfig.ts
@@ -3,9 +3,9 @@ export const initAnchor = {
     y: 1
 };
 
-export const officeMarkerImg = require("@/assets/images/office-marker.png");
-export const roomMarkerImg = require("@/assets/images/room-marker.png");
-export const toilerMarkerImg = require("@/assets/images/toilet-marker.png");
-export const canteenMarkerImg = require("@/assets/images/canteen-marker.png");
-export const swimmingMarkerImg = require("@/assets/images/swimming-marker.png");
-export const gymMarkerImg = require("@/assets/images/gym-marker.png");
+export const officeMarkerImg = require("@/assets/images/markers/office-marker.png");
+export const roomMarkerImg = require("@/assets/images/markers/room-marker.png");
+export const toilerMarkerImg = require("@/assets/images/markers/toilet-marker.png");
+export const canteenMarkerImg = require("@/assets/images/markers/canteen-marker.png");
+export const swimmingMarkerImg = require("@/assets/images/markers/swimming-marker.png");
+export const gymMarkerImg = require("@/assets/images/markers/gym-marker.png");
