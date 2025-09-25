bb5a82cc28394485c979f8e9bdfa649b7180222c
furantuturu <furantuturu@proton.me>
2025-09-25 18:18:36 +0800

    added the enh version of area markers and added image text markers

--- constants/markerConfig.ts
+++ constants/markerConfig.ts
@@ -3,9 +3,17 @@ export const initAnchor = {
     y: 1
 };
 
-export const officeMarkerImg = require("@/assets/images/markers/office-marker.png");
-export const roomMarkerImg = require("@/assets/images/markers/room-marker.png");
-export const toilerMarkerImg = require("@/assets/images/markers/toilet-marker.png");
-export const canteenMarkerImg = require("@/assets/images/markers/canteen-marker.png");
-export const swimmingMarkerImg = require("@/assets/images/markers/swimming-marker.png");
-export const gymMarkerImg = require("@/assets/images/markers/gym-marker.png");
+export const officeMarkerImg = require("@/assets/images/markers/office-marker-enh.png");
+export const roomMarkerImg = require("@/assets/images/markers/room-marker-enh.png");
+export const toilerMarkerImg = require("@/assets/images/markers/toilet-marker-enh.png");
+export const canteenMarkerImg = require("@/assets/images/markers/canteen-marker-enh.png");
+export const swimmingMarkerImg = require("@/assets/images/markers/swimming-marker-enh.png");
+export const gymMarkerImg = require("@/assets/images/markers/gym-marker-enh.png");
+
+export const gymNameMarker = require("@/assets/images/buildingnames-markers/Gym-Building.png");
+export const maritimeNameMarker = require("@/assets/images/buildingnames-markers/Maritime-Building.png");
+export const susanaNameMarker = require("@/assets/images/buildingnames-markers/Susana-Building.png");
+export const oldNameMarker = require("@/assets/images/buildingnames-markers/Old-Building.png");
+export const mareNameMarker = require("@/assets/images/buildingnames-markers/Mar-E-Shop.png");
+export const gate1NameMarker = require("@/assets/images/buildingnames-markers/Gate-1.png");
+export const gate2NameMarker = require("@/assets/images/buildingnames-markers/Gate-2.png");
