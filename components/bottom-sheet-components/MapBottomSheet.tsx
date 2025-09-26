2e4c58ec568dbbc587b06de19730ddf6a1ebf1bc
furantuturu <furantuturu@proton.me>
2025-09-27 00:47:43 +0800

    sub-area sheet is now dismissable

--- components/bottom-sheet-components/MapBottomSheet.tsx
+++ components/bottom-sheet-components/MapBottomSheet.tsx
@@ -2,15 +2,25 @@ import { TrueSheet } from "@lodev09/react-native-true-sheet";
 import { StyleSheet } from "react-native";
 
 //* Components
+import { useMyStoreV2 } from "@/store/useMyStore";
+import { useRef } from "react";
 import AreaSheetContent from "./AreaSheetContent";
 import FloorSheetContent from "./FloorSheetContent";
 
 export default function FloorBottomSheet() {
+    const mainSheetRef = useRef<TrueSheet>(null);
+    const subSheetRef = useRef<TrueSheet>(null);
+    const { setShowAreaSheet } = useMyStoreV2();
 
+    async function dismissAreaSheet() {
+        setShowAreaSheet(false);
+        await mainSheetRef.current?.present();
+    }
     return (
         <>
             <TrueSheet
                 name="main-sheet"
+                ref={mainSheetRef}
                 sizes={['11%', '43%', 'large']}
                 initialIndex={0}
                 initialIndexAnimated
@@ -22,11 +32,12 @@ export default function FloorBottomSheet() {
                 <FloorSheetContent />
                 <TrueSheet
                     name="sub-sheet"
+                    ref={subSheetRef}
                     sizes={['25%', '43%', 'large']}
-                    dismissible={false}
                     dimmed={false}
                     cornerRadius={24}
                     contentContainerStyle={styles.sheet}
+                    onDismiss={dismissAreaSheet}
                 >
                     <AreaSheetContent />
                 </TrueSheet>
