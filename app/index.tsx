d78955c80042299a53beb67da807cf31edcfd91e
furantuturu <furantuturu@proton.me>
2025-10-02 01:34:55 +0800

    shows certain components if isNavigating is true or false

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,26 @@
+import Map from "@/components/Map";
+import NavigationController from "@/components/NavigationController";
+import NonNavigationComponents from "@/components/NonNavigationComponents";
+import { useUserLocStore } from "@/store/useMyStore";
 import { StyleSheet, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-//* Components
-import FloorBottomSheet from "@/components/bottom-sheet-components/MapBottomSheet";
-import ActivateUserLocation from "@/components/fab/ActivateUserLocation";
-import FloorsSelectionPerCategoryModal from "@/components/fab/FloorsSelectionPerCategoryModal";
-import MapDimensionToggle from "@/components/fab/MapDimensionToggle";
-import SchoolCategoryButton from "@/components/fab/SchoolCategoryButton";
-import AreaSearchBar from "@/components/fab/SearchAreaModal";
-import Map from "@/components/marker-components/Map";
-
 export default function Index() {
+  const { isNavigating } = useUserLocStore();
+
+
   return (
     <PaperProvider>
       <SafeAreaProvider>
         <View style={styles.container}>
 
           <Map />
-
-          <AreaSearchBar />
-          <SchoolCategoryButton />
-          <MapDimensionToggle />
-          <FloorsSelectionPerCategoryModal />
-          <ActivateUserLocation />
+          {isNavigating
+            ? <NavigationController />
+            : <NonNavigationComponents />
+          }
         </View>
-        <FloorBottomSheet />
       </SafeAreaProvider>
     </PaperProvider>
   );
