d70278f1086b4608471a4fa4534ce6a294bc2156
furantuturu <furantuturu@proton.me>
2025-10-03 01:26:47 +0800

    collective components when isNavgating is true

---
+++ components/NavigationComponents.tsx
@@ -0,0 +1,9 @@
+import NavigationController from "./NavigationController";
+
+export default function NavigationComponents() {
+    return (
+        <>
+            <NavigationController />
+        </>
+    );
+}
