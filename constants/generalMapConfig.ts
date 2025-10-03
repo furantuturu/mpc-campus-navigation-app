4576f5d2126958155ae947a79efb3525be3ef6fe
furantuturu <furantuturu@proton.me>
2025-10-03 22:38:42 +0800

    replace school named to campus

--- constants/generalMapConfig.ts
+++ constants/generalMapConfig.ts
@@ -7,14 +7,14 @@ export const IMAGE_BG_OVERLAY_COORDS: [Position, Position, Position, Position] =
     [125.178199, 6.108200],
     [125.124949, 6.088551]
 ];
-export const SCHOOL_BG_IMAGE_URL = require("@/assets/images/mpcbgpic.webp");
+export const CAMPUS_BG_IMAGE_URL = require("@/assets/images/mpcbgpic.webp");
 export const IMAGE_OVERLAY_COORDS: [Position, Position, Position, Position] = [
     [125.14544362902387, 6.11816384690516],
     [125.1437233331672, 6.117520354279671],
     [125.14420364535526, 6.116247657395669],
     [125.14592652924557, 6.116885371310816]
 ];
-export const SCHOOL_IMAGE_URL = require("@/assets/images/mpcpic.webp");
+export const CAMPUS_IMAGE_URL = require("@/assets/images/mpcpic.webp");
 export const CAMERA_BOUNDS: CameraBounds = {
     ne: [125.14372494489749, 6.116191725240824],
     sw: [125.1459859184514, 6.118166860054188]
