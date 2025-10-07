ed82741e54e980216d8fe711384ed49b8687ffac
furantuturu <furantuturu@proton.me>
2025-10-07 22:44:48 +0800

    to access panorama images

---
+++ constants/explorePanoramas.ts
@@ -0,0 +1,17 @@
+export const exploreModePanoramas: any = {
+    exp1: require(`@/assets/images/explore-pano/exp1.webp`),
+    exp2: require(`@/assets/images/explore-pano/exp2.webp`),
+    exp3: require(`@/assets/images/explore-pano/exp3.webp`),
+    exp4: require(`@/assets/images/explore-pano/exp4.webp`),
+    exp5: require(`@/assets/images/explore-pano/exp5.webp`),
+    exp6: require(`@/assets/images/explore-pano/exp6.webp`),
+    exp7: require(`@/assets/images/explore-pano/exp7.webp`),
+    exp8: require(`@/assets/images/explore-pano/exp8.webp`),
+    exp9: require(`@/assets/images/explore-pano/exp9.webp`),
+    exp10: require(`@/assets/images/explore-pano/exp10.webp`),
+    exp11: require(`@/assets/images/explore-pano/exp11.webp`),
+    exp12: require(`@/assets/images/explore-pano/exp12.webp`),
+    exp13: require(`@/assets/images/explore-pano/exp13.webp`),
+    exp14: require(`@/assets/images/explore-pano/exp14.webp`),
+    exp15: require(`@/assets/images/explore-pano/exp15.webp`)
+};
