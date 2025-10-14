9d7b08ab85f64cbd2960eaf6e6deab19585a813e
furantuturu <furantuturu@proton.me>
2025-10-14 23:52:05 +0800

    added AreaSheetContent and BottomSheetToggle

--- components/NonNavigationComponents.tsx
+++ components/NonNavigationComponents.tsx
@@ -3,6 +3,8 @@ import CampusCategoryButton from "@/components/fab/CampusCategoryButton";
 import FloorsSelectionPerCategoryModal from "@/components/fab/FloorsSelectionPerCategoryModal";
 import MapDimensionToggle from "@/components/fab/MapDimensionToggle";
 import AreaSearchBar from "@/components/fab/SearchAreaModal";
+import AreaSheetContent from "./bottom-sheet-components/AreaSheetContent";
+import BottomSheetToggle from "./fab/BottomSheetToggle";
 
 export default function NonNavigationComponents() {
     return (
@@ -12,6 +14,8 @@ export default function NonNavigationComponents() {
             <CampusCategoryButton />
             <MapDimensionToggle />
             <FloorsSelectionPerCategoryModal />
+            <BottomSheetToggle />
+            <AreaSheetContent />
         </>
     );
 }
